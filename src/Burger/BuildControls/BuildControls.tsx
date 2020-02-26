a9f431423b17d196e76310632f7a6b5caa22f520
Junyu Zhu <junyu.zhu@alliedtelesis.co.nz>
2020-02-26 18:01:44 +1300

    Add order button styles

--- src/Burger/BuildControls/BuildControls.tsx
+++ src/Burger/BuildControls/BuildControls.tsx
@@ -22,7 +22,7 @@ const BuildControls = (props: any) => (
         disabled={props.disabled[ctrl.type]}
       />
     ))}
-    <button>ORDER NOW</button>
+    <button className={styles.OrderButton}>ORDER NOW</button>
   </div>
 );
 
