44fa77163ec8be8eeeecbfee6cf6f5d78ebbd565
Junyu Zhu (J.Y) <zhujunyu168@hotmail.com>
2020-03-16 21:39:17 +1300

    Convert menu to hamburger icon

--- src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.tsx
+++ src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.tsx
@@ -1,5 +1,12 @@
 import React from "react";
+import * as styles from "./DrawerToggle.css";
 
-const DrawerToggle = (props: any) => <div onClick={props.clicked}>MENU</div>;
+const DrawerToggle = (props: any) => (
+  <div className={styles.DrawerToggle} onClick={props.clicked}>
+    <div></div>
+    <div></div>
+    <div></div>
+  </div>
+);
 
 export default DrawerToggle;
