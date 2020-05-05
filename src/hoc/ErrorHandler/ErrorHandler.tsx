5ac5b0fd97c4473d155567548a6906f48aa89296
Junyu Zhu <zhujunyu168@hotmail.com>
2020-05-06 09:02:21 +1200

    Unset interceptors when the component unmount

--- src/hoc/ErrorHandler/ErrorHandler.tsx
+++ src/hoc/ErrorHandler/ErrorHandler.tsx
@@ -1,20 +1,27 @@
 import React, { Component } from "react";
 import Auxiliary from "../Auxiliary/Auxiliary";
 import Modal from "../../components/UI/Modal/Modal";
-import { AxiosInstance, AxiosRequestConfig } from "axios";
+import {
+  AxiosInstance,
+  AxiosRequestConfig
+} from "axios";
 
 export const ErrorHandler = (WrappedComponent: any, axios: AxiosInstance) => {
   return class extends Component {
     state = {
-      error: ''
+      error: ""
     } as any;
+    reqInterceptor: number = 0;
+    respInterceptor: number = 0;
 
     componentWillMount(): void {
-      axios.interceptors.request.use((req: AxiosRequestConfig) => {
-        this.setState({ error: null });
-        return req;
-      });
-      axios.interceptors.response.use(undefined, err => {
+      this.reqInterceptor = axios.interceptors.request.use(
+        (req: AxiosRequestConfig) => {
+          this.setState({ error: null });
+          return req;
+        }
+      );
+      this.respInterceptor = axios.interceptors.response.use(undefined, err => {
         this.setState({ error: err });
       });
     }
@@ -23,10 +30,18 @@ export const ErrorHandler = (WrappedComponent: any, axios: AxiosInstance) => {
       this.setState({ error: null });
     };
 
+    componentWillUnmount(): void {
+      axios.interceptors.request.eject(this.reqInterceptor);
+      axios.interceptors.request.eject(this.respInterceptor);
+    }
+
     render() {
       return (
         <Auxiliary>
-          <Modal show={this.state.error} closeModal={this.errorConfirmedHandler}>
+          <Modal
+            show={this.state.error}
+            closeModal={this.errorConfirmedHandler}
+          >
             {this.state.error ? this.state.error.message : null}
           </Modal>
           <WrappedComponent {...this.props} />
